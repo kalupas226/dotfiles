ce9e68cc8c6c69ea32459df665175d25b38583c3
kalupas226 <kalupas226@gmail.com>
2025-07-09 08:32:25 +0900

    Fix keymaps

--- packages/nvim/.config/nvim/lua/config/base-keymaps.lua
+++ packages/nvim/.config/nvim/lua/config/base-keymaps.lua
@@ -1,8 +1,6 @@
 -- Key mappings
 local map = vim.keymap.set
 
--- Insert mode mappings (bracket completion handled by nvim-autopairs)
-
 -- Visual mode mappings
 map("v", ">", ">gv")
 map("v", "<", "<gv")
@@ -10,6 +8,9 @@ map("v", "<", "<gv")
 -- Normal mode mappings
 map("n", "<C-h>", ":bprev<CR>", { silent = true })
 map("n", "<C-l>", ":bnext<CR>", { silent = true })
+map("n", "<leader>bd", ":bdelete<CR>", { silent = true, desc = "Delete buffer" })
+map("n", "<leader>bw", ":bwipeout<CR>", { silent = true, desc = "Wipeout buffer" })
+map("n", "<leader>bl", ":buffers<CR>", { silent = true, desc = "List buffers" })
 
 -- Command mode mappings
 map("c", "<C-p>", "<Up>")
